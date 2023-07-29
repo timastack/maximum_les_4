6409c2501c11163cb33076b5042d24da8c705fdb
тима <timochist123@gmail.com>
2023-07-29 15:57:00 +0300

    домашка 4 лекция

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
